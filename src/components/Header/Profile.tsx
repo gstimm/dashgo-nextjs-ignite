55c68f9bdcc911556f212cfd48de6889590308f5
gstimm <contato.gabrieltimm@gmail.com>
2021-09-02 15:15:06 -0300

    feat: add responsiveness to Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Gabriel Timm</Text>
-        <Text color='gray.300' fontSize='small'>
-          contato.gabrieltimm@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Gabriel Timm</Text>
+          <Text color='gray.300' fontSize='small'>
+            contato.gabrieltimm@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size='md'
