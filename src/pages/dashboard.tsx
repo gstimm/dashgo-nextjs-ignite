efd99bcc5718626f2165568fdd35ad917e394595
gstimm <contato.gabrieltimm@gmail.com>
2021-09-02 10:34:25 -0300

    feat: add Sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
 import { Header } from '../components/Header';
+import { SideBar } from '../components/SideBar';
+import { Flex } from '@chakra-ui/react';
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex direction='column' h='100vh'>
+      <Header />
+
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+        <SideBar />
+      </Flex>
+    </Flex>
+  );
 }
